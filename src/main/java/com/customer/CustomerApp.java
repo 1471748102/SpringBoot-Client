ee307e0e1d3e5a679f71133d32d628b11c5a2793
TabEsc <1471748102@qq.com>
2020-02-14 17:41:41 +0800

    后端API接口程序编写+使用EasyCode和lombok工具进行操作

--- src/main/java/com/customer/CustomerApp.java
+++ src/main/java/com/customer/CustomerApp.java
@@ -1,13 +1,17 @@
 package com.customer;
 
+
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 
-@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@SpringBootApplication
+@MapperScan("com.customer.dao")
 public class CustomerApp {
-    public static void main(String[] agrs){
-        SpringApplication.run(CustomerApp.class,agrs);
+
+    public  static  void main(String[] agrs) {
+
+        SpringApplication.run (CustomerApp.class,agrs);
     }
 }
---
+++ src/main/java/com/customer/service/CustomerTableService.java
@@ -0,0 +1,14 @@
+package com.customer.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.customer.entity.CustomerTable;
+
+/**
+ * (CustomerTable)表服务接口
+ *
+ * @author makejava
+ * @since 2020-02-14 16:54:33
+ */
+public interface CustomerTableService extends IService<CustomerTable> {
+
+}
