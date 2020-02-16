5839221799dc9f04fdb8fd2e30c2a0fc240b22bb
TabEsc <1471748102@qq.com>
2020-02-16 21:47:23 +0800

    增加和删除功能

--- src/main/java/com/customer/controller/CustomerTableController.java
+++ src/main/java/com/customer/controller/CustomerTableController.java
@@ -3,14 +3,16 @@ package com.customer.controller;
 
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.api.ApiController;
 import com.baomidou.mybatisplus.extension.api.R;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.customer.entity.CustomerTable;
 import com.customer.service.CustomerTableService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import javax.annotation.Resource;
+
 import java.io.Serializable;
 import java.util.List;
 
@@ -20,14 +22,10 @@ import java.util.List;
  * @author makejava
  * @since 2020-02-14 16:54:33
  */
-@RestController
-@RequestMapping("customerTable")
-public class CustomerTableController extends ApiController {
-    /**
-     * 服务对象
-     */
-    @Resource
-    private CustomerTableService customerTableService;
+public class CustomerTableController {
+
+    @Autowired
+    CustomerTableService customerTableService;
 
     /**
      * 分页查询所有数据
@@ -37,8 +35,9 @@ public class CustomerTableController extends ApiController {
      * @return 所有数据
      */
     @GetMapping
-    public R selectAll(Page<CustomerTable> page, CustomerTable customerTable) {
-        return success(this.customerTableService.page(page, new QueryWrapper<>(customerTable)));
+    @CrossOrigin
+    public R<IPage<CustomerTable>>  selectAll(Page<CustomerTable> page, CustomerTable customerTable) {
+        return R.ok (this.customerTableService.page(page, new QueryWrapper<>(customerTable)));
     }
 
     /**
@@ -48,8 +47,9 @@ public class CustomerTableController extends ApiController {
      * @return 单条数据
      */
     @GetMapping("{id}")
-    public R selectOne(@PathVariable Serializable id) {
-        return success(this.customerTableService.getById(id));
+    @CrossOrigin
+    public R<CustomerTable> selectOne(@PathVariable Serializable id) {
+        return R.ok(this.customerTableService.getById(id));
     }
 
     /**
@@ -59,8 +59,11 @@ public class CustomerTableController extends ApiController {
      * @return 新增结果
      */
     @PostMapping
-    public R insert(@RequestBody CustomerTable customerTable) {
-        return success(this.customerTableService.save(customerTable));
+    @CrossOrigin
+    public R<Integer> insert(@RequestBody CustomerTable customerTable) {
+        boolean rs = this.customerTableService.save(customerTable);
+
+        return R.ok(rs?customerTable.getId():0);
     }
 
     /**
@@ -70,8 +73,10 @@ public class CustomerTableController extends ApiController {
      * @return 修改结果
      */
     @PutMapping
-    public R update(@RequestBody CustomerTable customerTable) {
-        return success(this.customerTableService.updateById(customerTable));
+    @CrossOrigin
+    public R<Integer>  update(@RequestBody CustomerTable customerTable) {
+        boolean rs = this.customerTableService.updateById(customerTable);
+        return R.ok(customerTable.getId());
     }
 
     /**
@@ -81,7 +86,8 @@ public class CustomerTableController extends ApiController {
      * @return 删除结果
      */
     @DeleteMapping
-    public R delete(@RequestParam("idList") List<Long> idList) {
-        return success(this.customerTableService.removeByIds(idList));
+    @CrossOrigin
+    public R<Boolean> delete(@RequestParam("idList") List<Long> idList) {
+        return R.ok(this.customerTableService.removeByIds(idList));
     }
 }
---
+++ src/main/java/com/customer/controller/CustomerTableExtendController.java
@@ -0,0 +1,11 @@
+package com.customer.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("customerTable")
+public class CustomerTableExtendController extends CustomerTableController {
+
+
+}
